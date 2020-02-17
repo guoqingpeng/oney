0df5b396cc95688da5f9bf61b97f8596605762a0
guoqingpeng <xiaogqp@163.com>
2020-02-17 18:04:14 +0800

    oney

---
+++ byy/byy.go
@@ -0,0 +1,12 @@
+package byy
+
+import "fmt"
+func ByVersion()  int{
+	fmt.Println("ByVersion called")
+	return 101
+}
+
+func Bygass()  int{
+	fmt.Println("Bygass called")
+	return 120
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module oney
+
+go 1.13
